73e34dded802b357152e39539dff531680dae463
Saroj Shrestha <saroj.shr911@gmail.com>
2024-11-09 15:32:26 +0000

    Update middleware for debug and added .gitignore

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,12 +1,19 @@
-const config = {
-    github_client_id: `${env.GITHUB_CLIENT_ID}`,
-    github_client_secret: `${env.GITHUB_CLIENT_SECRET}`,
-    oauth_host: "https://github.com",
-    oauth_token_path: "/login/oauth/access_token",
-    oauth_authorize_path: "/login/oauth/authorize",
-};
 
 export async function onRequest({ request, env, next }) {
+    const config = {
+        github_client_id: `${env.GITHUB_CLIENT_ID}`,
+        github_client_secret: `${env.GITHUB_CLIENT_SECRET}`,
+        oauth_host: "https://github.com",
+        oauth_token_path: "/login/oauth/access_token",
+        oauth_authorize_path: "/login/oauth/authorize",
+    };
+    
+    console.log("Available environment variables:", {
+        vars: Object.keys(env),
+        clientIdExists: !!env.GITHUB_CLIENT_ID,
+        clientIdValue: env.GITHUB_CLIENT_ID?.substring(0, 4) + "..." // Show first 4 chars only
+    });
+
     const url = new URL(request.url);
 
     // Handle callback from GitHub
